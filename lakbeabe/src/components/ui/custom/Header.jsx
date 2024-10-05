065d106c1f09c2c0688ef8c9baf6f270a399bb6c
klstxii <ksarmiento@student.hau.edu.ph>
2024-10-05 15:28:52 +0800

    initial frontend
    Co-authored-by: MANUEL JR. MENDEZ <memendez2@student.hau.edu.ph>

---
+++ lakbeabe/src/components/ui/custom/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from '../button'
+
+// Header Content
+function Header() {
+  return (
+    <div className='p-2 shadow-sm flex justify-between items-center px-5'>
+        <img src='/logo.svg'/>
+        <div>
+            <Button>Sign In</Button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ lakbeabe/src/constants/options.jsx
@@ -0,0 +1,52 @@
+// Option selections for the travel plan
+export const SelectTravelList=[
+    {
+        id:1,
+        title:'Solo',
+        desc:'Me, myself, and I',
+        icon:':)',
+        people:'1',
+    },
+    {
+        id:2,
+        title:'Duo',
+        desc:'It takes two',
+        icon:':))',
+        people:'2 People',
+    },
+    {
+        id:3,
+        title:'Trio',
+        desc:'Two plus one equals 3',
+        icon:':3',
+        people:'3 People',
+    },
+    {
+        id:4,
+        title:'Group Travel',
+        desc:'Bringing the whole group together',
+        icon:'(:',
+        people:'4+ people',
+    }
+]
+
+export const SelectBudgetOption=[
+    {
+        id:1,
+        title:'Cheap',
+        desc:'Okane ga nai',
+        icon:':(',
+    },
+    {
+        id:2,
+        title:'Average',
+        desc:'Salary Budget',
+        icon:':|',
+    },
+    {
+        id:3,
+        title:'Rich',
+        desc:'No Limits',
+        icon:':)',
+    }
+]
