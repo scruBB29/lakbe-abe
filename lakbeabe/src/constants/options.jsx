3e3b31443714be0c0bb23531aec63687ecbc1858
scruBB29 <mjmendez0529201@gmail.com>
2024-10-14 18:20:08 +0800

    feat: book hotel pricing

--- lakbeabe/src/constants/options.jsx
+++ lakbeabe/src/constants/options.jsx
@@ -1,3 +1,5 @@
+import { PhilippinePeso } from "lucide-react"
+
 // Option selections for the travel plan
 export const SelectTravelList=[
     {
@@ -51,4 +53,4 @@ export const SelectBudgetOption=[
     }
 ]
 
-export const AI_PROMPT='Generate Travel Plan for Location : {location}, for {totalDays} Days for {traveler} with a {budget} budget, give me Hotels options list with HotelName, Hotel address, Price, hotel image url, geo coordinates, rating, descriptions and  suggest itinerary with placeName, Place Details, Place Image Url, Geo Coordinates,Place address, ticket Pricing, Time travel each of the location for {totalDays} days with each day plan with best time to visit in JSON format.'
+export const AI_PROMPT='Generate Travel Plan for Location : {location}, for {totalDays} Days for {traveler} with a {budget} budget (in pesos), give me Hotels options list with HotelName, Hotel address, Price (in PhP)(only 1 value), hotel image url, geo coordinates, rating, descriptions and  suggest itinerary with placeName, Place Details, Place Image Url, Geo Coordinates,Place address, ticket Pricing, Time travel each of the location for {totalDays} days with each day plan with best time to visit in JSON format.'
