ba953636f8a5baf59b271d7a1e28daa3f30335e8
arj3l <tayagrijel20@gmail.com>
2024-10-14 14:47:20 +0800

    style: landing page and buttons style

--- lakbeabe/src/App.jsx
+++ lakbeabe/src/App.jsx
@@ -12,11 +12,11 @@ function App() {
   const [count, setCount] = useState(0)
   
   return (
-    <>
+    <div className="bg-image">
       {/* Hero */}
       <Hero />
       <Hotelbooking />
-    </>
+    </div>
   )
 }
 
