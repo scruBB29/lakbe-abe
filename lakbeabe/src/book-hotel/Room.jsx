a888bf4545ed944b91d2a8fba5ea1fbbd34cf3f6
arj3l <tayagrijel20@gmail.com>
2024-10-14 12:27:12 +0800

    feat: hotel booking front end 2

--- lakbeabe/src/book-hotel/Room.jsx
+++ lakbeabe/src/book-hotel/Room.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 function Room() {
-//   return (
-//     <div>
-//       {/* <h1>{Room.name}</h1> */}
-//     </div>
-//   )
+  return (
+    <div>
+      {/* <h1>{Room.name}</h1> */}
+    </div>
+  )
 }
 
 export default Room
